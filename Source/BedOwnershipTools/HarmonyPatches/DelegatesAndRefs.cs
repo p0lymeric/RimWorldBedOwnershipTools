71baf5ee540007c343d33184e8612f74f965dfbe
polymeric <229394044+p0lymeric@users.noreply.github.com>
2025-09-14 18:21:23 -0400

    Use Harmony 2.3.3 for RimWorld 1.5 compiles

--- Source/BedOwnershipTools/HarmonyPatches/DelegatesAndRefs.cs
+++ Source/BedOwnershipTools/HarmonyPatches/DelegatesAndRefs.cs
@@ -27,7 +27,7 @@ namespace BedOwnershipTools {
             // ThingWithComps.GetInspectString (non-virtual call)
             public delegate string MethodDelegateNonVirtual_ThingWithComps_GetInspectString(ThingWithComps thiss);
             public static MethodDelegateNonVirtual_ThingWithComps_GetInspectString NonVirtual_ThingWithComps_GetInspectString =
-                AccessTools.MethodDelegate<MethodDelegateNonVirtual_ThingWithComps_GetInspectString>(AccessTools.Method(typeof(ThingWithComps), nameof(ThingWithComps.GetInspectString)), null, false, null);
+                AccessTools.MethodDelegate<MethodDelegateNonVirtual_ThingWithComps_GetInspectString>(AccessTools.Method(typeof(ThingWithComps), nameof(ThingWithComps.GetInspectString)), virtualCall: false);
 
             // Pawn_Ownership.intOwnedBed
             public static AccessTools.FieldRef<Pawn_Ownership, Building_Bed> Pawn_Ownership_intOwnedBed =
