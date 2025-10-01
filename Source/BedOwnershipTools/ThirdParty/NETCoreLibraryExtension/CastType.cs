0c0c709fe04e70e3b16a8011d543a598b8593656
polymeric <229394044+p0lymeric@users.noreply.github.com>
2025-09-30 21:04:36 -0400

    Convert hard tabs in delegate helper library

--- Source/BedOwnershipTools/ThirdParty/NETCoreLibraryExtension/CastType.cs
+++ Source/BedOwnershipTools/ThirdParty/NETCoreLibraryExtension/CastType.cs
@@ -7,15 +7,15 @@
 
 namespace BedOwnershipTools.Whathecode.System.Reflection
 {
-	public enum CastType
-	{
-		/// <summary>
-		/// Only consider implicit conversions.
-		/// </summary>
-		Implicit,
-		/// <summary>
-		/// Consider all possible conversions within the same hierarchy which are possible using an explicit cast.
-		/// </summary>
-		SameHierarchy
-	}
+    public enum CastType
+    {
+        /// <summary>
+        /// Only consider implicit conversions.
+        /// </summary>
+        Implicit,
+        /// <summary>
+        /// Consider all possible conversions within the same hierarchy which are possible using an explicit cast.
+        /// </summary>
+        SameHierarchy
+    }
 }
--- Source/BedOwnershipTools/ThirdParty/NETStandardLibraryExtension/Variable.cs
+++ Source/BedOwnershipTools/ThirdParty/NETStandardLibraryExtension/Variable.cs
@@ -7,22 +7,22 @@
 
 namespace BedOwnershipTools.Whathecode.System
 {
-	/// <summary>
-	/// Helper class to do common operations on variables.
-	/// </summary>
-	public static class Variable
-	{
-		/// <summary>
-		/// Swaps the value of one variable with another.
-		/// </summary>
-		/// <typeparam name = "T">The type of the variables.</typeparam>
-		/// <param name = "first">The first value.</param>
-		/// <param name = "second">The second value.</param>
-		public static void Swap<T>( ref T first, ref T second )
-		{
-			T intermediate = first;
-			first = second;
-			second = intermediate;
-		}
-	}
+    /// <summary>
+    /// Helper class to do common operations on variables.
+    /// </summary>
+    public static class Variable
+    {
+        /// <summary>
+        /// Swaps the value of one variable with another.
+        /// </summary>
+        /// <typeparam name = "T">The type of the variables.</typeparam>
+        /// <param name = "first">The first value.</param>
+        /// <param name = "second">The second value.</param>
+        public static void Swap<T>( ref T first, ref T second )
+        {
+            T intermediate = first;
+            first = second;
+            second = intermediate;
+        }
+    }
 }
